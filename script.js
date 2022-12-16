a84f6f79b3a8a3f1b2aafdb84b2a55d45506266a
virtualp1 <zininnikita309@gmail.com>
2022-12-16 16:03:10 +0900

    v0.2.0

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ const PreviewCard = {
       type: Object,
       required: true,
     },
+    isGos: {
+      type: Boolean,
+      default: false,
+    },
+    isDk: {
+      type: Boolean,
+      default: false,
+    },
   },
   template: `
     <div class="card mb-3">
       <div class="row g-0">
+<!--        <div -->
+<!--          class="recommended" -->
+<!--          v-if="isGos" -->
+<!--          :class="{ 'col-md-12': isGos, 'col-md-4': !isGos }"-->
+<!--        >-->
+<!--          Рекомендуем-->
+<!--        </div>-->
         <div class="col-md-4">
           <img 
             :src="place.image" 
@@ -16,12 +31,27 @@ const PreviewCard = {
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">{{ place.title }}</h5>
+            <h5 class="card-title" :class="{flexible: isGos}">
+              {{ place.title }}
+              
+              <span 
+                v-if="isGos"
+                class="preview-card__badge"
+              >
+                Рекомендуем
+              </span>
+              
+              <span class="float-end" v-if="place.isNeedPay">
+                <img src="https://www.freepnglogos.com/uploads/star-png/star-vector-png-transparent-image-pngpix-21.png" alt="" width="24px" height="24px" />
+              </span>
+            </h5>
+            
             <p class="card-text">
-                {{ place.description }}
+              {{ place.description }}
             </p>
 
-            <a href='PreviewCard.html' class="btn btn-success">Посмотреть</a>
+            <a v-if="isDk" href="./PreviewCardDk.html" class="btn btn-success">Посмотреть</a>
+            <a v-else href="./PreviewCard.html" class="btn btn-success">Посмотреть</a>
           </div>
         </div>
       </div>
@@ -36,25 +66,13 @@ const SupportBubble = {
     };
   },
   template: `
-     <div class="support-bubble" :class="{ 'support-bubble--mini': isMini }">
+     <div @click="isMini = !isMini" class="support-bubble" :class="{ 'support-bubble--mini': isMini }">
       <div class="support-bubble__header">
         Полезные ресурсы
-
-        <button class="support-bubble__toggle" @click="isMini = !isMini">
-          {{ isMini ? '△' : '▽' }}
-        </button>
       </div>
       <div class="support-bubble__body">
         <div class="support-bubble__section">
-          <div class="support-bubble__section-title">Заводы:</div>
-          <div class="support-bubble__section-links">
-            <a href="">Амурский ГХК</a>
-            <a href="">Амурский ГПЗ</a>
-          </div>
-        </div>
-
-        <div class="support-bubble__section">
-          <div class="support-bubble__section-title">Кинотеатры:</div>
+          <div class="support-bubble__section-title">Градообразующие предприятия:</div>
           <div class="support-bubble__section-links">
             <a href="">Амурский ГХК</a>
             <a href="">Амурский ГПЗ</a>
@@ -65,8 +83,84 @@ const SupportBubble = {
   `,
 };
 
+const InfoButton = {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+
+  template: `
+    <div 
+      @mouseover="isShow = true" 
+      @mouseleave="isShow = false" 
+      class="header__info"
+    >
+      <img
+          width="18px"
+          height="18px"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Infobox_info_icon.svg/480px-Infobox_info_icon.svg.png"
+          alt=""
+      />
+      
+      <div v-if="isShow" class="header__info-content">{{ text }}</div>
+    </div>
+  `,
+};
+
+const UsefulBlock = {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  template: `
+    <div class="useful-block">
+      <img
+        class="useful-block__image"
+        :src="image"
+        width="18px"
+        height="18px"
+        alt=""
+      />
+  
+      <div class="useful-block__title">
+        {{ title }}
+  
+        <div class="useful-block__link">
+          <a
+            :href="link"
+            target="_blank"
+            class="useful-block__link"
+          >
+            Узнать подробнее
+          </a>
+        </div>
+      </div>
+    </div>
+  `,
+};
+
 Vue.component("preview-card", PreviewCard);
 Vue.component("support-bubble", SupportBubble);
+Vue.component("info-button", InfoButton);
+Vue.component("useful", UsefulBlock);
 
 new Vue({
   el: "#app",
@@ -74,49 +168,106 @@ new Vue({
     return {
       places: [
         {
-          title: "Амурлаг",
-          image:
-            "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-          description:
-            "Амурлаг амурлаг амурлаг амурлаг амурлаг амурлаг амурлаг",
+          title: "АГХК",
+          image: "./img/aghk1.jpg",
+          description: "Одно из главных градообразующих предприятий",
+          isGos: false,
+          isDk: false,
+          type: "factory",
+          isNeedPay: true,
         },
         {
-          title: "Амурлаг",
-          image:
-            "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-          description:
-            "Амурлаг амурлаг амурлаг амурлаг амурлаг амурлаг амурлаг",
+          title: "ДК ЖД",
+          image: "./img/dk.jpg",
+          description: `ДК ЖД - значимое место для жителей города Свободный.`,
+          isGos: false,
+          isDk: true,
+          type: "culture",
         },
         {
-          title: "Амурлаг",
+          title: "Госуслуги",
           image:
-            "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-          description:
-            "Амурлаг амурлаг амурлаг амурлаг амурлаг амурлаг амурлаг",
+            "https://gazetavyborg.ru/userfls/news/news_cover_slider/5/47694_gosuslugi-predostavlyayut-vo.jpg",
+          description: "Универсальный рекомендательный портал",
+          isGos: true,
+          isDk: false,
         },
         {
-          title: "Амурлаг",
+          title: "ГПЗ",
+          image: "./img/aghk2.jpg",
+          description: "Одно из главных градообразующих предприятий",
+          isGos: false,
+          isDk: false,
+          type: "factory",
+        },
+      ],
+
+      placesInfo: [
+        {
+          title: "АГХК",
+          image: "./img/aghk1.jpg",
+          description: "Одно из главных градообразующих предприятий",
+          isGos: false,
+          isDk: false,
+          type: "factory",
+          isNeedPay: true,
+        },
+        {
+          title: "ДК ЖД",
+          image: "./img/dk.jpg",
+          description: `ДК ЖД - значимое место для жителей города Свободный.`,
+          isGos: false,
+          isDk: true,
+          type: "culture",
+        },
+        {
+          title: "Госуслуги",
           image:
-            "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-          description:
-            "Амурлаг амурлаг амурлаг амурлаг амурлаг амурлаг амурлаг",
+            "https://gazetavyborg.ru/userfls/news/news_cover_slider/5/47694_gosuslugi-predostavlyayut-vo.jpg",
+          description: "Универсальный рекомендательный портал",
+          isGos: true,
+          isDk: false,
+        },
+        {
+          title: "ГПЗ",
+          image: "./img/aghk2.jpg",
+          description: "Одно из главных градообразующих предприятий",
+          isGos: false,
+          isDk: false,
+          type: "factory",
         },
       ],
 
       imagesInSwiper: [
-        "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-        "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-        "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-        "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-        "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-        "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
-        "https://lh3.googleusercontent.com/p/AF1QipNJGhi5azvn1rLyltliJ2GBwxCcYhy3NfGXzy9-=s680-w680-h510",
+        "./img/dk-poster.jpg",
+        "./img/kino-poster.jpg",
+        "./img/memory-poster.jpg",
+        "./img/pam-poster.jpg",
+        "./img/house-poster.jpg",
+        "./img/house2-poster.jpg",
+        "./img/fok-poster.jpg",
+        "./img/kino2-poster.jpg",
+        "./img/zhd-poster.jpg",
       ],
+
+      showDropdown: false,
     };
+  },
+
+  methods: {
+    sort(tag) {
+      if (tag === "all") {
+        this.places = this.placesInfo;
+
+        return;
+      }
+
+      this.places = this.placesInfo.filter((place) => place.type === tag);
+    },
   },
 });
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper-main", {
   direction: "horizontal",
   loop: true,
   slidesPerView: 6,
@@ -149,6 +300,11 @@ const swiper = new Swiper(".swiper", {
       spaceBetween: 16,
     },
   },
+});
+
+const swiperPlace = new Swiper(".swiper-places", {
+  direction: "horizontal",
+  slidesPerView: 1.6,
 });
 
 timer = setInterval(() => {
